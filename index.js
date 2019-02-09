aed6e486e06b8ddbb7203b3225d50ad597bc8dbc
nicolas <nicolas.matteazzi@gmail.com>
2019-02-09 18:54:16 +0100

    Go back to ConsumerGroup

--- index.js
+++ index.js
@@ -2,14 +2,7 @@ var kafka = require('kafka-node');
 var moment = require('moment-timezone');
 var logger = require('toto-logger');
 
-var client = new kafka.KafkaClient({
-  kafkaHost: 'kafka:9092',
-  connectTimeout: 10000,
-  requestTimeout: 6000
-});
-
-var Consumer = kafka.Consumer;
-
+var ConsumerGroup = kafka.ConsumerGroup;
 
 
 /**
@@ -37,7 +30,14 @@ class TotoEventConsumer {
     });
 
     // Create the Kafka Consumer Group
-    this.consumer = new Consumer(client, [{topic: topic, offset: 'latest', partition: 0}], {groupId: microservice, autoCommit: true, fetchMaxBytes: 100000000, fromOffset: true});
+    this.consumer = new ConsumerGroup({
+      kafkaHost: 'kafka:9092',
+      groupId: microservice,
+      protocol: ['roundrobin'],
+      fromOffset: 'latest',
+      commitOffsetsOnFirstJoin: true,
+      outOfRangeOffset: 'latest'
+    }, topic);
 
     // React to error messages
     this.consumer.on('error', (error) => {
@@ -49,20 +49,7 @@ class TotoEventConsumer {
     // This kind of error occurs when for example Kafka deletes the logs (based on the retention policy)
     // and the offset refers to deleted logs
     this.consumer.on('offsetOutOfRange', (error) => {
-      console.log('Error: OffsetOutOfRange for topics ' + this.topics[0].topicName);
-
-      var offset = new kafka.Offset(client);
-
-      offset.fetch([{ topic: this.topics[0].topicName, partition: 0, time: -1, maxNum: 1 }], (err, data) => {
-
-        let currentOff = data[this.topics[0].topicName]['0'][0];
-
-        this.consumer.pause();
-        this.consumer.setOffset(this.topics[0].topicName, 0, currentOff);
-        this.consumer.commit((err, data) => {if (err) console.log(err);});
-        this.consumer.resume();
-
-      });
+      console.log('Error: OffsetOutOfRange');
     })
 
     /**
